47d931ae5fb5f8ecc296e6389c7c8950de20f351
runtee <ekwo.chinonso.01@gmail.com>
2022-03-20 09:23:02 +0100

    first commit

---
+++ public/js/home.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to
+250px */ 
+
+function openNav() {
+    document.getElementById("sidebar")
+    .style.display = "block";
+   }
+function closeSide() {
+    document.getElementById("sidebar")
+    .style.display = "none";
+   }
